53541f27b2b5256c481f4f3fefa87d98fab809f6
Alexander-3112 <ajewe3112@gmail.com>
2023-08-04 14:33:21 +0700

    Initial Commit

---
+++ VoiceARRobot/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  VoiceARRobot
+//
+//  Created by Alexander Jason W on 04/08/23.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
